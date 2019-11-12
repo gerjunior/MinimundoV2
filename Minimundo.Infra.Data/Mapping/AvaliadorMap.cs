0f5bd3093b3d267b60187753f7f273b07f33fba4
abner silva <abnerluisplay2@hotmail.com>
2019-11-12 11:00:02 -0300

    Mapeamento da classe Avaliador

--- Minimundo.Infra.Data/Mapping/AvaliadorMap.cs
+++ Minimundo.Infra.Data/Mapping/AvaliadorMap.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Minimundo.Domain.Entities;
 using System;
 
 namespace Minimundo.Infra.Data.Mapping
 {
-    public class AvaliadorMap
+    public class AvaliadorMap : IEntityTypeConfiguration<Avaliador>
     {
         public void Configure(EntityTypeBuilder<Avaliador> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("Avaliador");
+
+            builder.HasKey(k => k.AvaliadorID);
+
+            builder.Property(p => p.UsuarioID)
+                   .HasColumnName("UsuarioID")
+                   .HasColumnType("int")
+                   .IsRequired();
+
+            builder.Property(p => p.CriadoPor)
+                   .HasColumnName("CriadoPor")
+                   .HasColumnType("varchar")
+                   .HasMaxLength(255)
+                   .IsRequired();
+
+            builder.Property(p => p.CriadoEm)
+                   .HasColumnName("CriadoEm")
+                   .HasColumnType("date")
+                   .IsRequired();
+
+            builder.Property(p => p.ModificadoPor)
+                   .HasColumnName("ModificadoPor")
+                   .HasColumnType("varchar")
+                   .HasMaxLength(255)
+                   .IsRequired();
+
+            builder.Property(p => p.ModificadoEm)
+                   .HasColumnName("ModificadoEm")
+                   .HasColumnType("date")
+                   .IsRequired();
+
+            builder.Property(p => p.Ativo)
+                   .HasColumnName("Ativo")
+                   .HasColumnType("boolean")
+                   .IsRequired();
         }
     }
 }
