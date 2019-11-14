eae54b09648fb0a51da87eae45149b08849654ca
Geraldo <geraldojuni@hotmail.com>
2019-11-14 11:54:18 -0300

    Alteração nas validações de CPF e CNPJ

--- Minimundo.Infra.CrossCutting/Constants/ValidatorConst.cs
+++ Minimundo.Infra.CrossCutting/Constants/ValidatorConst.cs
@@ -8,18 +8,85 @@ namespace Minimundo.Infra.CrossCutting
         public const string Vazio = "Obrigatório preencher as informações do campo";
         public const string Maximo = "A quantidade máxima de caracteres para o campo";
 
-        public static bool CNPJ(string CNPJ)
+        public static bool CNPJ(string cnpj)
         {
             Regex rgx = new Regex(@"[0 - 9]{ 2}\.?[0-9]{3}\.?[0 - 9]{3}\/?[0 - 9]{4}\-?[0 - 9]{2}");
 
-            return rgx.IsMatch(CNPJ);
+            if (!rgx.IsMatch(cnpj))
+                return false;
+
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
         }
 
         public static bool CPF(string CPF)
         {
             Regex rgx = new Regex(@"[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}");
 
-            return rgx.IsMatch(CPF);
+            if (!rgx.IsMatch(CPF))
+                return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            CPF = CPF.Trim();
+            CPF = CPF.Replace(".", "").Replace("-", "");
+            if (CPF.Length != 11)
+                return false;
+            tempCpf = CPF.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return CPF.EndsWith(digito);
         }
 
         public static bool Senha(string Senha)
